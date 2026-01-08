aecdccf9ffef87931156e01c6afbd8f49637d7f1
unitcodewave <unitcodewave@gmail.com>
2026-01-08 10:59:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair'); 
+let code = require('./pair');
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
 app.use('/code', code);
 app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
+    res.sendFile(__path + '/pair.html');
 });
 app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
+    res.sendFile(__path + '/main.html');
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
+// ✅ Changed here to bind on 0.0.0.0
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`
-𝚙𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚒𝚛 𝙻𝙾𝙵𝚃
+Don't Forget To Give Star ‼️
 
+𝐏𝙾𝚆𝙴𝚁𝙴𝙳 𝐁𝚈 𝙼𝙰𝙻𝚅𝙸𝙽 𝚇𝙳
 
-Server running on http://localhost:` + PORT)
+Server running on http://0.0.0.0:` + PORT);
 });
 
 module.exports = app;
